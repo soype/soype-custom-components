e4b8980c53384129b8ba64977e5d5de21340a9ce
Pedro Henestroza <phenestroza@gmail.com>
2025-09-11 18:22:16 -0300

    Replace background position

--- templates/richtext.php
+++ templates/richtext.php
@@ -5,8 +5,8 @@
 /** @var string $alignment **/
 /** @var string $color **/
 ?>
-<div class="soype-richtext <?php echo esc_attr($class); ?>">
-    <div class="soype-richtext__container soype-richtext--align-<?php echo esc_attr($alignment) ?>" style="background-color: <?php echo esc_attr($color) ?>;">
+<div class="soype-richtext <?php echo esc_attr($class); ?>" style="background-color: <?php echo esc_attr($color) ?>;">
+    <div class="soype-richtext__container soype-richtext--align-<?php echo esc_attr($alignment) ?>">
         <?php if ( ! empty($title) ): ?>
             <h3 class="soype-richtext__title"><?php echo esc_html($title); ?></h3>
         <?php endif; ?>
